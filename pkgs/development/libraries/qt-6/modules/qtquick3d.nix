d7262fbad2f6cea633e527718fcbd3c1faf7e45c
Aleksander Gondek <gondekaleksander@protonmail.com>
2021-12-05 14:56:05 +0100

    Make qtquick3d build

--- pkgs/development/libraries/qt-6/modules/qtquick3d.nix
+++ pkgs/development/libraries/qt-6/modules/qtquick3d.nix
@@ -3,11 +3,21 @@
 , libglvnd, libxkbcommon, vulkan-headers # TODO should be inherited from qtbase
 , qtdeclarative # TODO verify
 , openssl
+, qttest
+, pkg-config
+, zlib
 }:
 
 qtModule {
   pname = "qtquick3d";
-  qtInputs = [ qtbase qtdeclarative ];
-  buildInputs = [ openssl openssl.dev libglvnd libxkbcommon vulkan-headers ];
+  qtInputs = [ qttest ];
+  buildInputs = [ openssl openssl.dev libglvnd libxkbcommon vulkan-headers zlib pkg-config ];
   outputs = [ "out" "dev" "bin" ];
+  configureFlags = [
+    "-verbose"
+    "-pkg-config"
+    "-system-zlib"
+    "-L" "${zlib.out}/lib"
+    "-I" "${zlib.dev}/include"
+  ];
 }
